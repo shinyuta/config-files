1c3feca7fd45cb5e28c23b1feaa44a463702190a
shinyuta <shinyoota@gmail.com>
2024-12-03 11:47:38 -0600

    Colors.sh and front_app.sh changes (bar transparancy)

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -8,7 +8,7 @@ export DIM=0xff575279
 # export ITEM_BG_COLOR=0xff393552
 # export ACCENT_COLOR=0xffebbcba
 
-export BAR_COLOR=0xff100f14
+export BAR_COLOR=0x00100f14
 export ITEM_BG_COLOR=0xff393552
 export ACCENT_COLOR=0xfffd77dd
 export BLUE=0xff54c1db
--- sketchybar/items/front_app.sh
+++ sketchybar/items/front_app.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+# change icon.color and label.color to full opacity
 sketchybar --add item front_app left \
            --set front_app       background.color=$BLUE \
-                                 icon.color=$BAR_COLOR \
+                                 icon.color=0xff100f14 \
                                  icon.font="sketchybar-app-font:Regular:16.0" \
-                                 label.color=$BAR_COLOR \
+                                 label.color=0xff100f14 \
                                  script="$PLUGIN_DIR/front_app.sh"            \
            --subscribe front_app front_app_switched
