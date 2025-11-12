62290b1ddc1775362c550bf860f455b0555a773b
shinyuta <shinyoota@gmail.com>
2025-11-12 14:39:17 -0600

    Updates to Aerospace and etc.

--- sketchybar/items/month.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item month left \
-           --set month icon=󰃭 \
-                          label="$(date +'%a %b %d')" \
-                          update_freq=30 \
-                          script="$PLUGIN_DIR/month.sh"
--- sketchybar/plugins/time_popup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source "$CONFIG_DIR/colors.sh"
-
-sketchybar --set "$NAME" \
-  label="$(date +'%a %b %d')" \
-  label.color="$DIM" \
-  icon.color="$DIM"
